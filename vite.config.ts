c8e0bcb209097355a4b04ff328a41703a7683c9a
Kodai-Java <kodai8250@gmail.com>
2025-03-25 22:53:43 +0900

    vite tailwindcssインストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // storybookはViteプラグインを除外して使用する
 const isStorybook = process.argv[1]?.includes("storybook");
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     !isStorybook &&
       remix({
+        ignoredRouteFiles: ["**/*.css"],
         future: {
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
